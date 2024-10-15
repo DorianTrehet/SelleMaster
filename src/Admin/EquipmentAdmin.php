2c16a98cd9cc1996479ea35206470793d265763b
Dorian Trehet <dorian.trehet@gmail.com>
2024-10-15 15:48:15 +0200

    Configuration EquipmentAdmin.php

--- src/Admin/EquipmentAdmin.php
+++ src/Admin/EquipmentAdmin.php
@@ -3,37 +3,131 @@
 // src/Admin/EquipmentAdmin.php
 namespace App\Admin;
 
-use App\Entity\Equipment;
+use App\Entity\Category;
+use App\Entity\Location;
+use App\Entity\Condition;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
-use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
-class EquipmentAdmin extends AbstractAdmin
+final class EquipmentAdmin extends AbstractAdmin
 {
-    // Configuration du formulaire d'édition/création
     protected function configureFormFields(FormMapper $form): void
     {
-        $form->add('name')
-             ->add('category')
-             ->add('price')
-             ->add('lastMovement');
+        $form->add('name', TextType::class)
+            ->add('description', TextareaType::class, [
+                'required' => false,
+            ])
+            ->add('price', NumberType::class, [
+                'scale' => 2, // nombre de décimales
+            ])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+            ])
+            ->add('stat', EntityType::class, [
+                'class' => Condition::class,
+                'choice_label' => 'name',
+            ])
+            // Ajouter des champs pour saisir manuellement l'aisle et le shelf
+            ->add('location.aisle', TextType::class, [
+                'label' => 'Aisle', // Label pour le champ Aisle
+            ])
+            ->add('location.shelf', TextType::class, [
+                'label' => 'Shelf', // Label pour le champ Shelf
+            ])
+            ->add('lastMovement', DateTimeType::class, [
+                'widget' => 'single_text',
+                'required' => false,
+            ])
+            ->add('createdAt', DateTimeType::class, [
+                'widget' => 'single_text',
+                'disabled' => true,
+            ]);
     }
 
-    // Configuration des filtres pour la recherche
     protected function configureDatagridFilters(DatagridMapper $datagrid): void
     {
         $datagrid->add('name')
-                 ->add('category');
+                ->add('category', null, [
+                    'field_type' => EntityType::class,
+                    'field_options' => [
+                        'class' => Category::class,
+                        'choice_label' => 'name',
+                    ],
+                ])
+                ->add('stat', null, [
+                    'field_type' => EntityType::class,
+                    'field_options' => [
+                        'class' => Condition::class,
+                        'choice_label' => 'name',
+                    ],
+                ]);
     }
 
-    // Configuration de l'affichage dans la liste
     protected function configureListFields(ListMapper $list): void
     {
-        $list->addIdentifier('id')
-             ->add('name')
-             ->add('category')
-             ->add('price')
-             ->add('lastMovement');
+        $list->addIdentifier('name')
+            ->add('category.name', 'text', [
+                'label' => 'Category',
+            ])
+            ->add('stat.name', 'text', [
+                'label' => 'State',
+            ])
+            ->add('location.aisle', 'text', [
+                'label' => 'Aisle',
+            ])
+            ->add('location.shelf', 'text', [
+                'label' => 'Shelf',
+            ])
+            ->add('description', 'text', [ // Pas de tri sur cette colonne
+                'label' => 'Description',
+                'sortable' => false, // Désactiver le tri
+            ])
+            ->add('price')
+            ->add('lastMovement')
+            ->add('createdAt', 'date', [
+                'pattern' => 'dd MMM y G',
+                'locale' => 'fr',
+                'timezone' => 'Europe/Paris',
+            ]);
     }
+
+    protected function configureShowFields(ShowMapper $show): void
+    {
+        $show->add('name')
+            ->add('category.name', null, [
+                'label' => 'Category',
+            ])
+            ->add('stat.name', null, [
+                'label' => 'State',
+            ])
+            ->add('location.aisle', 'text', [
+                'label' => 'Aisle',
+            ])
+            ->add('location.shelf', 'text', [
+                'label' => 'Shelf',
+            ])
+            ->add('description', 'text', [ // Pas de tri sur cette colonne
+                'label' => 'Description',
+                'sortable' => false, // Désactiver le tri
+            ])
+            ->add('price')
+            ->add('lastMovement')
+            ->add('createdAt', 'date', [
+                'pattern' => 'dd MMM y G',
+                'locale' => 'fr',
+                'timezone' => 'Europe/Paris',
+            ]);
+    }
+
 }
+
+
